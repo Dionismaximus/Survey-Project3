c5d43c21ce558e83825c1201dad312fb669d9750
Denis Litvinenko <dionismaximus666@gmail.com>
2024-05-19 22:54:16 +0000

    Add few print statements with greeting and rules

--- run.py
+++ run.py
@@ -16,4 +16,18 @@ results = SHEET.worksheet('results')
 
 data = results.get_all_values()[1]
 
-print(data)
+print("------------------")
+print("We are pleased to welcome you to the video game popularity survey. Your answers are very important to us!")
+print("------------------")
+print("*** SURVEY RULES ***")
+print('Please enter only the number corresponding to your correct answer for each individual question and press the ENTER.\nIf you have read the rules and are ready to start the survey, enter 1 and press ENTER.')
+
+
+def survey_question():
+    """
+    List of questions which is displayed in the console for the user.
+    """
+    question1 = input('How often do you play games?\n1 - Everyday\n2 - A few times per week\n3 - A few times per month\n4 - Few times per year\n')
+    print(question1)
+
+survey_question()
