ebcbbe159599743b6cca7757d03c077a60c2c865
Denis Litvinenko <dionismaximus666@gmail.com>
2024-05-21 18:41:10 +0000

    Add main logic to survey_question function

--- run.py
+++ run.py
@@ -14,20 +14,43 @@ SHEET = GSPREAD_CLIENT.open('Popular-Games-Survey')
 
 results = SHEET.worksheet('results')
 
-data = results.get_all_values()[1]
+data = results.get_all_values()[0][0]
 
+'''
 print("------------------")
 print("We are pleased to welcome you to the video game popularity survey. Your answers are very important to us!")
 print("------------------")
 print("*** SURVEY RULES ***")
 print('Please enter only the number corresponding to your correct answer for each individual question and press the ENTER.\nIf you have read the rules and are ready to start the survey, enter 1 and press ENTER.')
-
+'''
 
 def survey_question():
     """
     List of questions which is displayed in the console for the user.
     """
-    question1 = input('How often do you play games?\n1 - Everyday\n2 - A few times per week\n3 - A few times per month\n4 - Few times per year\n')
-    print(question1)
+    user_answers = []
+    results_sheet = SHEET.worksheet('results')
+    questions = results_sheet.get_all_values()
+    
+    # Question1
+    question1 = questions[0][0]
+    question1_user_reply1 = questions[-1][0]
+
+    question1_option1 = '1 - Everyday'
+    question1_option2 = '2 - A few times per week'
+    question1_option3 = '3 - A few times per month'
+    question1_option4 = '4 - A few times per year'
+
+    user_reply1 = int(input(f'{question1}\n{question1_option1}\n{question1_option2}\n{question1_option3}\n{question1_option4}\n----------\n'))
+    if user_reply1 == 1:
+        user_answers.append(question1_option1)
+    elif user_reply1 == 2:
+        user_answers.append(question1_option2)
+    elif user_reply1 == 3:
+        user_answers.append(question1_option3)
+    else:
+        user_answers.append(question1_option4)
+    
+    print(user_answers)
 
 survey_question()
